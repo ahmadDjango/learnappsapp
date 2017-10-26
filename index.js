2a6085708ce953e709d0073d9e56bedd4bfe1855
ahmaddjango <tabba.ahmad@gmail.com>
2017-10-26 18:21:45 -0500

    changed index js and html

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
 var express = require('express');
-var bodyParser =require('body-parser');
 var app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/public/" + "index.html" );
+})
 
-app.get('/',function(req,res){
-res.sendFile(__dirname + '/public/index.html');
-});
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
 
-app.post('/login', function(req, res){
-  var adminPass = 'password123';
-  
-  var username =   req.body.username;
-  var password =   req.body.password;
-  
-  if(password === adminPass){
-    res.redirect('/success');
-  }
-  else
-  {
-    res.redirect('/failed');
-  }
-  
-});
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
 
-app.get('/success', function(req,res){
-  res.send('Login Successful!');
-});
-app.get('/failed', function(req,res){
-  res.send('Invalid Username or pass');
-});
-
-
-app.listen(8080, function(){
-  console.log('server is running on port 8080');
-});
+})
