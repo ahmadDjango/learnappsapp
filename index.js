fab54edb94b579495f9fa039a98546150dfbfe03
Tabbakha, Ahmad <Ahmad.Tabbakha@tylertech.com>
2018-05-02 13:26:10 -0500

    examples of auto conversion

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-app.get('/index.html', function (req, res) {
-   res.sendFile( __dirname + "/public/" + "index.html" );
-})
-
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-
-})
+console.log("" + 1 + 0)
+console.log("" - 1 + 0)
+console.log(true + false)
+console.log(6 / "3")
+console.log("2" * "3")
+console.log(4 + 5 + "px")
+console.log("$" + 4 + 5)
+console.log("4" - 2)
+console.log("4px" - 2)
+console.log(7 / 0)
+console.log("  -9\n" + 5)
+console.log("  -9\n" - 5)
+console.log(null + 1)
+console.log(undefined + 1)
