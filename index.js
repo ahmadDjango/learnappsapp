f6d7cabc5b2e489a8cd5337fc2ad6b10852b8eb0
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-12 12:46:52 -0500

    add mongoclient and mongo dep

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-var pug = require('pug')
-var app = require('express')()
+const pug = require('pug')
+const bodyParser = require('body-parser')
+const app = require('express')()
+const fs = require('fs')
+
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 // renderFile
 var html = pug.renderFile('start.pug');
+
+var db
+MongoClient.connect(dbConnString, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('ahmadstestdatabase')
+  app.listen(3000, function() {
+    console.log('listening on 3000')
+  })  
+})
+
 
 app.get('/', (req, res) => {
   res.status(200).send(html)
 })
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
+app.post('/quotes', (req,res) => {
 
+})
