85d623570224fec2260107b3ca3c4610cfbac9a1
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-11 23:04:03 -0500

    Render pug file either from cli or in node

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var ahmad = "ahmad"
+var pug = require('pug');
 
-console.log(ahmad)
-ahmad += 1
-console.log(ahmad)
-console.log(typeof(ahmad))
-console.log(ahmad.length )
-console.log(ahmad.charCodeAt(0) )
-console.log(ahmad.concat("wow","this") )
-console.log(ahmad.repeat(20))
+// renderFile
+var html = pug.renderFile('start.pug');
+
+console.log(html)
