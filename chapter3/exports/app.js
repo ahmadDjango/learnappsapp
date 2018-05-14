272a516c76fe1d523a2a6e70629dffbcec435477
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-14 17:40:05 -0500

    add exports filedir and nexttick examples

---
+++ chapter3/exports/app.js
@@ -0,0 +1,11 @@
+var boo1 = require('./boo1');
+boo1.a();
+boo1.b();
+
+var boo2 = require('./boo2');
+boo2.a();
+boo2.b();
+
+var boo3 = require('./boo3');
+boo3.a();
+boo3.b();
---
+++ chapter3/globals/nexttick.js
@@ -0,0 +1,9 @@
+process.nextTick(function () {
+  console.log('next tick');
+});
+
+console.log('immediate');
+console.log('immediate');
+console.log('immediate');
+console.log('immediate');
+console.log('immediate');
