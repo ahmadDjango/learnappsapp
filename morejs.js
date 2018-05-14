0e431d1fa7646464bff4b004a6b6362f01970cb9
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-13 19:58:15 -0500

    revealing module pattern

--- morejs.js
+++ morejs.js
@@ -1,22 +1,52 @@
 var foo;
-console.log(foo); // undefined
+console.log(foo);  // undefined
 
-var foo = { bar: 123 };
-console.log(foo.bar); // 123
-console.log(foo.bas); // undefined
+var foo = {bar: 123};
+console.log(foo.bar);  // 123
+console.log(foo.bas);  // undefined
 
 // string converted to number
 console.log('--EXACT EQUALITY--')
-console.log("5 == '5':", 5 == '5'); // true
-console.log("5 === '5':",5 === '5'); // false
+console.log('5 == \'5\':', 5 == '5');    // true
+console.log('5 === \'5\':', 5 === '5');  // false
 
 // Are these all falsy?
 if (!false) {
   console.log('falsy');
   }
-  if (!null) {
+if (!null) {
   console.log('falsy');
   }
-  if (!undefined) {
+if (!undefined) {
   console.log('falsy');
   }
+
+
+function printableMessage() {
+  var message = 'hello';
+  function setMessage(newMessage) {
+    if (!newMessage) throw new Error('cannot set empty message');
+    message = newMessage;
+  }
+  function getMessage() {
+    return message;
+  }
+  function printMessage() {
+    console.log(message);
+  }
+  return {
+    setMessage: setMessage,
+    getMessage: getMessage,
+    printMessage: printMessage
+  };
+}
+
+// Pattern in use
+var awesome1 = printableMessage();
+awesome1.printMessage();  // hello
+var awesome2 = printableMessage();
+awesome2.setMessage('hi');
+awesome2.printMessage();  // hi
+// Since we get a new object everytime we call the module function
+// awesome1 is unaffected by awesome2
+awesome1.printMessage();  // hello
