42c8cf12d6429a3ef94185697924abf41b7e35c4
Tabbakha, Ahmad <tabba.ahmad@gmail.com>
2018-05-15 23:06:16 -0500

    add comment explaining how to make the example work

--- chapter4/hello/foo.js
+++ chapter4/hello/foo.js
@@ -1,3 +1,5 @@
-var bar = require('bar') // bar was found in the parent node_modules folder
+// for this example to run you need to create a parent folder with node_modules
+// create a file and export a function that does anything in bar.js
+var bar = require('bar'); // bar was found in the parent node_modules folder
 
 bar();
